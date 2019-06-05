b2f60227e43a4482193b79c5488bea962d37883e
Abdo <abdopoetry@gmail.com>
2019-06-05 21:13:29 +0200

    Add current user time

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import getTime from './helpers/getTime';
 
 function App() {
   const backgroundImageSrc = `https://source.unsplash.com/random/${
@@ -9,14 +10,17 @@ function App() {
     <div
       style={{
         background: `linear-gradient(
-          rgba(0, 0, 0, 0.45), 
-          rgba(0, 0, 0, 0.45)
+          rgba(0, 0, 0, 0.60), 
+          rgba(0, 0, 0, 0.60)
         ),url(
           ${backgroundImageSrc}
         )`
       }}
       className="container"
-    />
+    >
+      <p className="userName">Hello, Abdo</p>
+      <p className="userTime">{getTime()}</p>
+    </div>
   );
 }
 
