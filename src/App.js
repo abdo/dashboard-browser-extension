89d2745b702350dff3d27f1ae5c1326ea3992ce2
Abdo <abdopoetry@gmail.com>
2019-06-05 21:00:50 +0200

    Add random background color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const backgroundImageSrc = `https://source.unsplash.com/random/${
+    window.innerWidth
+  }x${window.innerHeight}`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+      style={{
+        background: `linear-gradient(
+          rgba(0, 0, 0, 0.45), 
+          rgba(0, 0, 0, 0.45)
+        ),url(
+          ${backgroundImageSrc}
+        )`
+      }}
+      className="container"
+    />
   );
 }
 
