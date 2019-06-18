3ed18a922f72e529300b0d9dec5142116862077f
Abdo <abdopoetry@gmail.com>
2019-06-18 18:42:45 +0200

    Customize antd select to be a creatableSelect to select multiple themes for background image

--- src/components/SettingsModal/index.js
+++ src/components/SettingsModal/index.js
@@ -1,13 +1,15 @@
-import { Modal, Input, Radio, Switch } from 'antd';
-import React from 'react';
+import { Input, Modal, Radio, Select, Switch } from 'antd';
+import React, { useState } from 'react';
 
 import { defaultUserName } from '../../constants';
 
 import './style.css';
+import capitalize from '../../helpers/capitalize';
 
 const SettingsModal = ({ open, onChangeInput, onOk, onCancel, savedInfo }) => {
-  const capitalize = (word) =>
-    word[0].toUpperCase() + word.slice(1, word.length);
+  const [imgOption, setImgOption] = useState('');
+
+  console.log(imgOption);
   return (
     <Modal
       title="Settings"
@@ -23,7 +25,6 @@ const SettingsModal = ({ open, onChangeInput, onOk, onCancel, savedInfo }) => {
         onChange={(e) => onChangeInput('userName', e.target.value)}
         className="input"
       />
-
       <Radio.Group
         value={savedInfo.timeFormat}
         onChange={(e) => onChangeInput('timeFormat', e.target.value)}
@@ -32,13 +33,37 @@ const SettingsModal = ({ open, onChangeInput, onOk, onCancel, savedInfo }) => {
         <Radio.Button value="12">12 hours time format</Radio.Button>
         <Radio.Button value="24">24 hours time format</Radio.Button>
       </Radio.Group>
-
       <Switch
+        className="input"
         checkedChildren="Show bookmarks"
         unCheckedChildren="Hide bookmarks"
         checked={savedInfo.showBookmarks === 'true'}
         onChange={(value) => onChangeInput('showBookmarks', value.toString())}
       />
+      <p className="imgLabelsHeader">
+        <span role="img" aria-label="camera">
+          📷
+        </span>{' '}
+        I want to customize my background images:
+      </p>
+      <Select
+        addonBefore="Your Name"
+        mode="multiple"
+        style={{ width: '100%' }}
+        placeholder="For example: Smile"
+        defaultValue={['a10', 'c12']}
+        dropdownMenuStyle={{ height: 0 }}
+        onChange={(e) => {
+          console.log(e);
+        }}
+        onSearch={(input) => setImgOption(input)}
+      >
+        {imgOption && (
+          <Select.Option key={imgOption || 'imgOption'}>
+            {imgOption}
+          </Select.Option>
+        )}
+      </Select>
     </Modal>
   );
 };
